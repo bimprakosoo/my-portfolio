a749ee656ba2e717419a1c7b71180a00b9c27648
Bimo Prakoso <biimoprakosoo@gmail.com>
2023-06-23 19:34:36 +0700

    add skill, portfolio and contact

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,188 @@
     <NavBar/>
     <v-container fluid>
       <div class="head">
-
+        <v-row>
+          <v-col cols="5">
+            <div style="position: relative" class="mt-16">
+              <h1 class="text-grey">Hello,</h1>
+              <h1 class="text-white">I'm Bimo Prakoso</h1>
+              <span class="text-grey">Backend Developer</span><br/>
+              <v-btn tile dark class="text-yellow mt-8" variant="outlined">Contact me</v-btn>
+            </div>
+          </v-col>
+          <v-col cols="2">
+            <div style="
+                position: absolute;
+                z-index: 9999;
+                bottom: 0;
+                margin-left: auto;
+                margin-right: auto;
+                left: 0;
+                right: 0;
+                text-align: center;
+              " class="mt-16">
+              <v-icon>fas fa-angle-double-down</v-icon>
+            </div>
+          </v-col>
+          <v-col cols="5">
+            <div style="position: relative; z-index: 9999" class="mt-16">
+              <v-img src="i1.png" contain max-height="300"></v-img>
+            </div>
+          </v-col>
+        </v-row>
       </div>
+      <v-col cols="12" class="mt-16" id="about">
+        <div>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <div class="egg">
+                <v-img src="i2.png" max-height="300"></v-img>
+              </div>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <h5 class="mt-16">About Me</h5>
+              <div style="width: 120px">
+                <v-slider
+                  v-model="slider2"
+                  color="yellow"
+                ></v-slider>
+              </div>
+              <h4 class="mt-n4">I am a Backend Developer</h4>
+              <p class="text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <v-btn tile dark color="yellow" class="mt-4">
+                Download Resume
+              </v-btn>
+            </v-col>
+          </v-row>
+        </div>
+      </v-col>
+      <div class="text-center mt-4">
+        <h2>Skill</h2>
+        <div style="width:120px; margin: 0 auto">
+          <v-slider v-model="slider2" color="yellow"></v-slider>
+        </div>
+      </div>
+      <v-col cols="12" class="padd" id="skill">
+        <div class="first" id="project">
+          <v-row>
+            <v-col cols="12">
+              <div class="child">
+                <v-btn icon="fas fa-laptop" color="#F8DF7E" class="text-white"></v-btn>
+                <h3 class="ml-3 mt-4">Web Design</h3>
+                <p class="text-grey ml-3 mt-4 text-caption">
+                  Lorem, ipsum dolor sit amet <br/>consectetur adipisicing
+                  <br/>consectetur adipisicing
+                </p>
+              </div>
+            </v-col>
+          </v-row>
+          <v-divider></v-divider>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="12" id="portfolio">
+        <div class="text-center mt-4">
+          <h2>Portfolio</h2>
+          <div style="width:120px; margin: 0 auto">
+            <v-slider v-model="slider2" color="yellow"></v-slider>
+          </div>
+        </div>
+        <div class="d-flex justify-center mb-6">
+          <v-btn color="#FBDF7E" class="mr-2">All</v-btn>
+          <v-btn class="mr-2" variant="tonal">Web Design</v-btn>
+        </div>
+      </v-col>
+      <v-col cols="12" class="imgHover">
+        <v-row class="fill-height" align="center" justify="center">
+          <template v-for="(item, i) in items" :key="i">
+            <v-col cols="12" md="4">
+              <v-hover v-slot="{isHovering, props}">
+                <v-card :elevation="isHovering ? 12: 2" :class="{'on-hover' : isHovering}" v-bind="props">
+                  <v-img :src="item.img" height="225px" cover></v-img>
+                </v-card>
+              </v-hover>
+            </v-col>
+          </template>
+        </v-row>
+      </v-col>
+      <v-col cols="12" sm="12">
+        <div class="d-flex justify-center mb-6">
+
+        </div>
+      </v-col>
+      <v-col cols="12" id="biog">
+        <div class="hire">
+          <v-row>
+            <v-col cols="12" sm="8">
+              <h1 class="mt-9">Hire me for your awesome project</h1>
+              <p class="text-grey">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+                itaque, eaque molestiae deleniti, earum voluptate eos id dicta
+                at, blanditiis
+              </p>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <v-btn color="#FBDF7E" class="mt-15">Hire Me</v-btn>
+            </v-col>
+          </v-row>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="12" class="px-16" id="contact">
+        <v-row>
+          <v-col cols="12" sm="4">
+            <div class="child">
+              <h1>Contact info.</h1>
+              <v-btn
+                icon="fas fa-phone-alt"
+                color=""
+                class="mt-10"
+                variant="outlined"
+              ></v-btn
+              ><br />
+              <span class="text-caption">+6281331398067 </span> <br />
+              <v-btn
+                icon="fas fa-envelope"
+                color=""
+                class="mt-10"
+                variant="outlined"
+              ></v-btn
+              ><br />
+              <span class="text-caption">biimoprakosoo@gmail.com </span> <br />
+            </div>
+          </v-col>
+          <v-col cols="12" sm="8">
+            <h1 class="mt-8">Send your message</h1>
+            <v-divider></v-divider>
+            <span class="text-caption"
+            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              fugiat officia, odio eaque exercitationem libero nesciunt placeat,
+              repellat obcaecati sed tenetur! Est labore aliquam amet
+              consequatur necessitatibus fugit obcaecati facilis!</span
+            >
+            <v-row class="mt-10">
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  label="Name"
+                  persistent-hint
+                  variant="outlined"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-text-field
+                  label="Phone No"
+                  persistent-hint
+                  variant="outlined"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-textarea
+              label="Message"
+              persistent-hint
+              variant="outlined"
+            ></v-textarea>
+            <v-btn color="#FBDF7E" class="mt-2">Submit Now</v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
     </v-container>
   </v-app>
 </template>
@@ -17,7 +197,32 @@ import NavBar from "@/components/NavBar.vue";
 
 export default defineComponent({
   name: 'HomeView',
+  setup() {
+    return {
+      slider2: 50,
 
+      items: [
+        {
+          img: "haloteach.png",
+        },
+        {
+          img: "halokes.png",
+        },
+        {
+          img: "nitromax.png",
+        },
+        {
+          img: "autoglaze.png",
+        },
+        {
+          img: "united_tractor.png",
+        },
+        {
+          img: "jeruk_emas.png",
+        },
+      ],
+    };
+  },
   components: {
     NavBar,
   },
@@ -55,5 +260,44 @@ export default defineComponent({
   width: 50%;
   background: black;
   transform: skew(0deg, -6deg);
+}
+.egg {
+  display: block;
+  margin-left: 100px;
+  margin-top: 50px;
+  width: 356px;
+  height: 300px;
+  background-color: #FBDF7E;
+  border-radius: 50% 50% 50% 50% / 60% 60% 40% 0%;
+}
+.first {
+  width: 100%;
+  height: 280px;
+  text-align: center;
+  padding: 2rem 2rem;
+}
+.child {
+  display: inline-block;
+  padding: 2rem 1rem;
+  vertical-align: middle;
+  text-align: center;
+  margin-right: 8px;
+}
+.imgHover {
+  padding: 0 200px;
+}
+.pre {
+  width: 100%;
+  height: 380px;
+  text-align: center;
+  padding: 0 200px;
+  background-color: #F5F5F5;
+}
+.hire {
+  width: 100%;
+  height: 200px;
+  padding: 0 200px;
+  background-color: #E9E9E9;
+  margin-top: -24px;
 }
 </style>
