3a75621efdcf7848a3b1ff35fdb410e358b6f2b1
Bimo Prakoso <biimoprakosoo@gmail.com>
2023-06-26 14:47:44 +0700

    Adding social media icon and email function

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,10 @@
 <template>
-  <v-app-bar app color="black" dark flat class="px-12">
-    <v-btn>
-      <v-icon color="yellow" left class="mr-2">fas fa-signature</v-icon>Bimo
-    </v-btn>
-    <v-spacer></v-spacer>
+  <v-app-bar app color="black" dark flat class="px-12 custom-navbar">
+    <div class="d-flex justify-center w-100">
     <v-btn text @click="scroll('home')" class="text-yellow">Home</v-btn>
     <v-btn text @click="scroll('skill')">Skill</v-btn>
     <v-btn text @click="scroll('portfolio')">Portfolio</v-btn>
-    <v-btn text @click="scroll('contact')">Contact</v-btn>
+    </div>
   </v-app-bar>
 </template>
 
@@ -22,5 +19,8 @@ export default {
 }
 </script>
 <style scoped>
-
+.custom-navbar .v-btn {
+  display: flex;
+  justify-content: center;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,29 +4,16 @@
     <v-container fluid>
       <div class="head">
         <v-row>
-          <v-col cols="5">
+          <v-col cols="6">
             <div style="position: relative" class="mt-16">
               <h1 class="text-grey">Hello,</h1>
               <h1 class="text-white">I'm Bimo Prakoso</h1>
               <span class="text-grey">Backend Developer</span><br/>
               <v-btn tile dark class="text-yellow mt-8" variant="outlined">Download CV</v-btn>
+              <v-btn tile dark class="text-yellow mt-8" variant="outlined" @click="scroll('contact')" style="margin-left: 10px">Contact Me</v-btn>
             </div>
           </v-col>
-          <v-col cols="2">
-            <div style="
-                position: absolute;
-                z-index: 9999;
-                bottom: 0;
-                margin-left: auto;
-                margin-right: auto;
-                left: 0;
-                right: 0;
-                text-align: center;
-              " class="mt-16">
-              <v-icon>fas fa-angle-double-down</v-icon>
-            </div>
-          </v-col>
-          <v-col cols="5">
+          <v-col cols="6">
             <div style="position: relative; z-index: 9999" class="mt-16">
               <v-img src="avatar.png" contain max-height="300"></v-img>
             </div>
@@ -90,12 +77,12 @@
         <div class="d-flex justify-center mb-6">
           <v-btn :color="getButtonColor('All')" class="mr-2" @click="selectCategory('All')">All</v-btn>
           <v-btn :color="getButtonColor('Web')" class="mr-2" @click="selectCategory('Web')">Web</v-btn>
-          <v-btn :color="getButtonColor('Web')" class="mr-2" @click="selectCategory('Game')">Game</v-btn>
+          <v-btn :color="getButtonColor('Game')" class="mr-2" @click="selectCategory('Game')">Game</v-btn>
         </div>
       </v-col>
       <v-dialog v-model="dialog" max-width="1000px">
         <v-card>
-          <v-carousel hide-delimiters v-model="carouselIndex" style="height: auto !important">
+          <v-carousel hide-delimiters v-model="carouselIndex" style="height: 800px !important">
             <v-carousel-item v-for="(item, index) in carouselItems" :key="index" style="display: flex">
               <div>
                 <v-img :src="item.img" height="600px" contain></v-img>
@@ -123,80 +110,34 @@
 
         </div>
       </v-col>
-      <v-col cols="12" id="biog">
+      <v-col cols="12" id="contact">
         <div class="hire">
           <v-row>
-            <v-col cols="12" sm="8">
-              <h1 class="mt-9">Hire me for your awesome project</h1>
+            <v-col cols="12" sm="10">
+              <h1 class="mt-9 text-white">Hire me for your awesome project</h1>
               <p class="text-grey">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
-                itaque, eaque molestiae deleniti, earum voluptate eos id dicta
-                at, blanditiis
+                If you are interested in collaborating with me, please contact me via email.
               </p>
             </v-col>
-            <v-col cols="12" sm="4">
-              <v-btn color="#FBDF7E" class="mt-15">Hire Me</v-btn>
+            <v-col cols="12" sm="2">
+              <a :href="'mailto:biimoprakosoo@gmail.com'" class="mt-15">
+                <v-btn color="#FBDF7E" class="mt-15">Hire Me</v-btn>
+              </a>
             </v-col>
           </v-row>
         </div>
       </v-col>
-      <v-col cols="12" sm="12" class="px-16" id="contact">
-        <v-row>
-          <v-col cols="12" sm="4">
-            <div class="child">
-              <h1>Contact info.</h1>
-              <v-btn
-                icon="fas fa-phone-alt"
-                color=""
-                class="mt-10"
-                variant="outlined"
-              ></v-btn
-              ><br />
-              <span class="text-caption">+6281331398067 </span> <br />
-              <v-btn
-                icon="fas fa-envelope"
-                color=""
-                class="mt-10"
-                variant="outlined"
-              ></v-btn
-              ><br />
-              <span class="text-caption">biimoprakosoo@gmail.com </span> <br />
-            </div>
-          </v-col>
-          <v-col cols="12" sm="8">
-            <h1 class="mt-8">Send your message</h1>
-            <v-divider></v-divider>
-            <span class="text-caption"
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-              fugiat officia, odio eaque exercitationem libero nesciunt placeat,
-              repellat obcaecati sed tenetur! Est labore aliquam amet
-              consequatur necessitatibus fugit obcaecati facilis!</span
-            >
-            <v-row class="mt-10">
-              <v-col cols="12" sm="6">
-                <v-text-field
-                  label="Name"
-                  persistent-hint
-                  variant="outlined"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-text-field
-                  label="Phone No"
-                  persistent-hint
-                  variant="outlined"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-textarea
-              label="Message"
-              persistent-hint
-              variant="outlined"
-            ></v-textarea>
-            <v-btn color="#FBDF7E" class="mt-2">Submit Now</v-btn>
-          </v-col>
-        </v-row>
-      </v-col>
+      <div class="social-icons">
+        <v-btn icon href="https://gitlab.com/bimprakosoo" style="margin-bottom: 20px" target="_blank">
+          <v-icon icon="fa-brands fa-gitlab"></v-icon>
+        </v-btn>
+        <v-btn icon href="https://github.com/bimprakosoo" style="margin-bottom: 20px" target="_blank">
+          <v-icon icon="fa-brands fa-github"></v-icon>
+        </v-btn>
+        <v-btn icon href="https://www.linkedin.com/in/bimooprakosoo/" target="_blank">
+          <v-icon icon="fa-brands fa-linkedin"></v-icon>
+        </v-btn>
+      </div>
     </v-container>
   </v-app>
 </template>
@@ -265,6 +206,10 @@ export default defineComponent({
       this.carouselIndex = index;
       this.dialog = true;
     },
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({behavior: "smooth"});
+    }
   },
   components: {
     NavBar,
@@ -282,7 +227,6 @@ export default defineComponent({
   margin-bottom: 6px;
   height: 400px;
   width: 100%;
-  color: white;
  }
 .head:before {
   content: " ";
@@ -304,15 +248,6 @@ export default defineComponent({
   background: black;
   transform: skew(0deg, -6deg);
 }
-.egg {
-  display: block;
-  margin-left: 100px;
-  margin-top: 50px;
-  width: 356px;
-  height: 300px;
-  background-color: #FBDF7E;
-  border-radius: 50% 50% 50% 50% / 60% 60% 40% 0%;
-}
 .first {
   width: 100%;
   height: 280px;
@@ -329,23 +264,31 @@ export default defineComponent({
 .imgHover {
   padding: 0 200px;
 }
-.pre {
-  width: 100%;
-  height: 380px;
-  text-align: center;
-  padding: 0 200px;
-  background-color: #F5F5F5;
-}
+
 .hire {
   width: 100%;
   height: 200px;
   padding: 0 200px;
-  background-color: #E9E9E9;
+  background-color: #000000;
   margin-top: -24px;
 }
 .text-description {
   padding: 16px;
   font-size: 16px;
   text-align: center;
+}
+a:link {
+  color: #000000;
+}
+a:visited {
+  color: #000000;
+}
+.social-icons {
+  position: fixed;
+  bottom: 20px; /* Adjust the bottom position as needed */
+  left: 20px; /* Adjust the left position as needed */
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 }
 </style>
