85f6d5691fe33902de4174a88e6088863fe92598
Bimo Prakoso <biimoprakosoo@gmail.com>
2023-09-04 21:39:09 +0700

    add url for the project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -93,6 +93,7 @@
                 <v-img :src="item.img" height="600px" contain></v-img>
                 <div class="text-description">{{ item.description }}</div>
                 <div class="text-framework">{{ item.framework }}</div>
+                <div class="text-url"><a :href="item.url" target="_blank">Check Github / Link Project</a></div>
               </div>
             </v-carousel-item>
           </v-carousel>
@@ -176,37 +177,42 @@ export default defineComponent({
             'Haloteach is a website where teachers and students can engage in online learning. For teachers, there are several features such as creating classes for students to choose from, creating quizzes, ' +
             'and uploading learning materials. As for students, they can select classes they want to take, participate in quizzes provided by teachers, access free materials from the classes they enroll in, and provide ratings for ' +
             'teachers/classes they attend. Additionally, Haloteach collaborates with external tutoring services that wish to enter the world of online learning.',
-          framework: "Built with Codeigniter And MySQL"
+          framework: "Built with Codeigniter And MySQL", url: ""
         },
         { img: "halokes.png", category : "Web", description: '\n' +
             'Halokes is a website/application for school administration. In this system, the school can perform various tasks such as managing class schedules, the latest curriculum, teacher/student attendance, extracurricular activities, ' +
             'and more. On the other hand, students can submit assignments, view their grade history from past semesters, or check their attendance records.',
-          framework: "Built with Codeigniter and MySQL"
+          framework: "Built with Codeigniter and MySQL", url: ""
         },
         { img: "nitromax.png", category : "Web", description: 'Nitromax is a system designed to monitor nitrogen filling stations commonly found at gas stations. In addition to nitrogen filling, the system allows users to view sales data, such as tire repair transactions, ' +
             'oil sales, tire replacement services, and more. Clients can perform various tasks on the Nitromax website, such as viewing sales data, modifying prices for goods/services, adding new services/products, monitoring operator attendance, and more. In addition to the website, ' +
             'Nitromax utilizes a mobile application to connect the nitrogen machine to the Nitromax system.',
-          framework: "Built with Codeigniter and MySQL"
+          framework: "Built with Codeigniter and MySQL", url: ""
         },
         { img: "autoglaze.png", category : "Web", description: 'Autoglaze is a POS system for car wash and other transactions. The application utilizes a mobile app and a website to monitor car wash transactions. The car wash service provider can perform various ' +
             'actions using the application, such as inputting the car license plate, selecting services chosen by customers, adding desired extras, and more. Additionally, through the website system, the car wash service provider can monitor sales data, add new products/services, ' +
             'modify prices, create vouchers and promotions, and perform other related tasks.',
-          framework: "Built with Laravel and MySQL"
+          framework: "Built with Laravel and MySQL", url: ""
         },
         { img: "united_tractor.png", category : "Web", description: 'United Tractor is a website designed to monitor construction projects undertaken by United Tractor. It offers several features such as financial expense management, progress tracking for each construction project, a directory system, ' +
             'geolocation services, and more.',
-          framework: "Built with Codeigniter and MySQL"
+          framework: "Built with Codeigniter and MySQL", url: ""
+        },{ img: "track-task.png", category : "Web", description: 'A simple trello clone to track your task.',
+          framework: "Built with Next.js, React Drag and Drop, and Firebase", url: "https://simple-trello-theta.vercel.app/"
+        },
+        { img: "article-summarize.png", category : "Web", description: 'A simple application to summarize articles.',
+          framework: "Built with React.js, Vite, Tailwind.css, Redex, and RapidAPI", url: "https://article-summarize.vercel.app/"
         },
         { img: "jeruk_emas.png", category : "Game", description: 'Jeruk Emas is a Unity-based game where the player\'s objective is to find the golden orange while avoiding enemy pursuit. The enemy objects utilize two algorithms to chase the player. The first algorithm is A*, which finds the fastest path to the player. ' +
             'The second algorithm is FSM (Finite State Machine), which has multiple states for chasing the player.',
-          framework: "Built with Unity"
+          framework: "Built with Unity", url: ""
         },
         { img: "link-changer.png", category : "Discord", description: 'A Discord Bot that has function to automatically modified url video from twitter, tiktok, and instagram so it can be played on discord without click the link because sometimes those url ' +
             'got error and can not be played. It is using existing library from other people github, so the bot only change the url.',
-          framework: "Built with Node.js with discord.js plugin"
+          framework: "Built with Node.js with discord.js plugin", url: "https://github.com/bimprakosoo/link-changer-bot"
         },
         { img: "chatgpt.png", category : "Discord", description: 'A Discord Bot that can answer your question like chatGPT. Using gpt 3.5 as a model for this bot, now you can ask anything in your server.',
-          framework: "Built with Node.js with discord.js and openai"
+          framework: "Built with Node.js with discord.js and openai", url: "https://github.com/bimprakosoo/chatGPT-bot"
         },
       ],
       loadCount: 6,
@@ -322,6 +328,11 @@ export default defineComponent({
   text-align: justify;
 }
 .text-framework {
+  padding: 16px;
+  font-size: 18px;
+  text-align: center;
+}
+.text-url {
   padding: 16px;
   font-size: 18px;
   text-align: center;
